9a421c7eacb1654758a90978ed78b1ae05e6f89c
pstnv <pstnv@mail.ru>
2022-10-15 13:32:13 +0500

    fix(исправлено склонение слова идея в заголовке)

--- src/App.js
+++ src/App.js
@@ -6,28 +6,39 @@ import { things } from './things';
 function App () {
 
   const [list, setList] = useState(things);
+  const [idea, setIdea] = useState(declOfNum(list.length));
 
   const hideItem = (id) => {
     const listFiltered = list.filter(item => id !== item.id);
     setList(listFiltered);
+    setIdea(declOfNum(listFiltered.length));
   }
+
+  function declOfNum (number) {
+    const cases = [2, 0, 1, 1, 1, 2];
+    const titles = ['идея', 'идеи', 'идей'];
+    return titles[
+        number % 100 > 4 && number % 100 < 20 ? 2 : cases[number % 10 < 5 ? number % 10 : 5]
+    ];
+    };
+
 
 
   function  toggleList () {
-
     if (list.length === 0) {
       setList(things);
     }
     else {
       setList([]);
-    }
-
+    };
+    setIdea(declOfNum(list.length));
   }
+
   return (    
     <div className="main">
         <div className="container">
             <div className="things">
-                <h1 className="things_header">Как интересно провести время - {list.length} идей</h1>
+                <h1 className="things_header">Как интересно провести время - {list.length} {idea} </h1>
                 <p className="things_text">Конечно, ты можешь самостоятельно придумать способы провести время вместе, но если фантазия неожиданно отказала, воспользуйся этими подсказками.</p>
                 <div className="wrapper">
                   {list.map(element => {
